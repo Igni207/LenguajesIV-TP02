be8f06cb0f6cf5fd231ed6a262420f7d719064ad
Igni207 <juanirq2@gmail.com>
2026-09-14 16:24:41 -0300

    Update Inicio.jsx

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -5,8 +5,7 @@ function Inicio() {
     <section className="inicio">
       <h2>Inicio</h2>
       <p>
-        Bienvenido a nuestro sitio. Este es el Trabajo Práctico N°2 de la cátedra Lenguaje IV,
-        desarrollado con React y React Router.
+        Trabajo Práctico N°2 Lenguajes IV.
       </p>
     </section>
   )
